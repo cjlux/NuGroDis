92b498294043e0e040021f5bf3fb3a731fbae328
Jean-Luc Charles <jean-luc.charles@ensam.eu>
2015-03-01 19:13:42 +0100

    GPL headers in source file

--- libMetallurgical/SRC/Sprime.hpp
+++ libMetallurgical/SRC/Sprime.hpp
@@ -1,3 +1,17 @@
+// This file is part of NuGrodis, a workbench for micro-structural modelling.
+//
+// Author(s)    : - Jean-luc CHARLES, I2M/ENSAM Talence France
+//                  <jean-luc.charles@ensam.eu>
+//                - Dominique COUPARD, I2M/ENSAM Talence France
+//                  <dominique.coupardensam.eu>
+//                - Moubarak GADO, I2M/ENSAM Talence France
+//                  <gadomoubarak@yahoo.fr>
+//                - Ivan IORDANOFF, I2M/ENSAM Talence France
+//                  <ivan.iordanoff@ensam.eu>
+//
+// Copyright (C) 20014-2015 Jean-luc CHARLES, Dominique COUPARD, Moubarak GADO, Ivan IORDANOFF.
+//
+
 #include <string>
 
 //Precitate of type Sprime
